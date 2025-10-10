cc79d9262477ea8512dbd56666b1c47d84e3ec67
TheBestDeveloper <thebestdeveloper@outlook.fr>
2025-10-11 01:40:43 +0200

    Add initial command registration for Discord bot

---
+++ src/register.ts
@@ -0,0 +1,14 @@
+import { Command, Option, register } from 'discord-hono'
+
+const commands = [
+  new Command('someone', 'Ping a random member from the server').options(
+    new Option('ignore-bots', 'Ignore bot users', 'Boolean'),
+  ),
+  new Command('ping', 'Replies with the current ping, and other cool stuff!'),
+]
+
+register(
+  commands,
+  process.env.DISCORD_APPLICATION_ID,
+  process.env.DISCORD_TOKEN,
+)
