efd023d1a86132943f14f70048b51e1abad3fb3e
TheBestDeveloper <thebestdeveloper@outlook.fr>
2025-11-11 00:30:19 +0100

    📝 docs: Add GitHub link to help message
    Adds a link to the GitHub repository to the help message.
    
    - Provides users with easy access to the source code.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ const app = new DiscordHono()
 - \`/ping\`: Replies with the current ping.
 - \`/help\`: Provides help information for available commands.
 
-To use a command, type \`/\` followed by the command name. For example, to ping a random member, type \`/someone\`. You can add the optional parameter \`ignore-bots\` to exclude bot users from being selected.`;
+To use a command, type \`/\` followed by the command name. For example, to ping a random member, type \`/someone\`. You can add the optional parameter \`ignore-bots\` to exclude bot users from being selected.
+-# Source code is available on [GitHub](https://github.com/notthebestdev/someoneback).`;
 
 		return c.res(helpMessage);
 	})
